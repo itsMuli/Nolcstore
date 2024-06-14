fa9cfe5d0baf05a5f28eae1c151f4a99d42fc789
itsMuli <rosemwendwa163@gmail.com>
2024-06-14 20:36:02 +0300

    proxy reset

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Contact from './../../pages/Contact';
-import Policy from './../../pages/Policy';
 
 const Footer = () => {
   return (
@@ -10,7 +8,7 @@ const Footer = () => {
         <p className='text-center mt-3'>
           <Link to="/about">About</Link>
           <Link to='/contact'>Contact</Link>
-          <Link to='/policy'>Privacy Policy</Link>
+          <Link to='/policy'>Policy</Link>
         </p>
     </div>
   )
